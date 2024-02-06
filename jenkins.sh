0fd78e0a323cfa2f4a750055ae6eb9b0d021ea15
bkhemanth17 <146440776+bkhemanth17@users.noreply.github.com>
2024-02-06 15:47:35 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,11 @@
+#installing dependencies
+yum install git java-1.8.0-openjdk maven -y
+#installing java11
+amazon-linux-extras install java11-openjdk -y
+#installing jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+#start the service
+systemctl start jenkins.service
+systemctl status jenkins.service
