6452f0fc97071475cc40685c7142f254c0fa5e02
bkhemanth17 <146440776+bkhemanth17@users.noreply.github.com>
2024-02-06 16:12:18 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,7 @@
 yum install git java-1.8.0-openjdk maven -y
 #installing java11
 amazon-linux-extras install java11-openjdk -y
+update-alternatives --config java
 #installing jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
